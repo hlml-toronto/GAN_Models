2b30803d1ff0984d9b81579cc6bf29e772bb89dd
Matthew Smart <mattsmart@hotmail.ca>
2020-07-10 11:09:09 -0400

    save + load model blocks in ipynb

--- examples/mtgGAN/settings.py
+++ examples/mtgGAN/settings.py
@@ -3,6 +3,7 @@ import os
 DIR_IMAGES = 'Images'
 DIR_OUTPUT = 'Output'
 DIR_JSON = 'BULK_JSON'
+DIR_MODEL = 'Models'
 
 DEFAULT_GAN_DATA = DIR_IMAGES + os.sep + 'Creatures' + os.sep + 'Human'
 DEFAULT_GAN_OUTPUT = DIR_OUTPUT
